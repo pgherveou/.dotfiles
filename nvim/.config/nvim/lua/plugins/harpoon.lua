1aef338d2e5492fa4568925ce63046c6c7d41419
PG Herveou <pgherveou@lyft.com>
2022-03-31 10:11:32 +0200

    fixes mapping

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ local M = {}
 M.setup = function()
   vim.cmd([[
   :nnoremap <silent><leader>a :lua require("harpoon.mark").add_file()<CR>
-  :nnoremap <silent><C-d> :lua require("harpoon.ui").toggle_quick_menu()<CR>
+  :nnoremap <silent><C-h> :lua require("harpoon.ui").toggle_quick_menu()<CR>
   :nnoremap <silent><leader>tc :lua require("harpoon.cmd-ui").toggle_quick_menu()<CR>
   :nnoremap <silent><C-j> :lua require("harpoon.ui").nav_file(1)<CR>
   :nnoremap <silent><C-k> :lua require("harpoon.ui").nav_file(2)<CR>
