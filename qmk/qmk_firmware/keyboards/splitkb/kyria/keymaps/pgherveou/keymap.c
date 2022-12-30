1ea9816ce426507350d8dc15fc002d6a5cfdf1cb
PG herveou <pgherveou@lyft.com>
2022-12-30 18:50:16 +0100

    wip

--- qmk/qmk_firmware/keyboards/splitkb/kyria/keymaps/pgherveou/keymap.c
+++ qmk/qmk_firmware/keyboards/splitkb/kyria/keymaps/pgherveou/keymap.c
@@ -121,13 +121,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // clang-format on
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable = true;
-  debug_matrix = true;
-  // debug_keyboard=true;
-  // debug_mouse=true;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (!process_record_encoder(keycode, record)) {
