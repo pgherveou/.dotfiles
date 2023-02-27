b7631ddd81aa92eacdbb39efed738f995e0d16cf
PG herveou <pgherveou@lyft.com>
2023-02-27 12:01:55 +0100

    fix

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -347,18 +347,25 @@ local load_plugins = function(use)
   })
 end
 
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+-- automatically install and se up packer.nvim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup(plugins, opts)
+-- https://github.com/wbthomason/packer.nvim#bootstrapping
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use('wbthomason/packer.nvim')
+  load_plugins(use)
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
