ca81e505be16b1f01a412643161eb7ffe0ec51bd
PG Herveou <pgherveou@gmail.com>
2023-12-20 09:55:02 +0100

    fzf update

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -9,7 +9,7 @@ fi
 # on macOS use base_path /opt/homebrew/opt/fzf/shell, on Debian use /usr/share/fzf/examples/
 
 OS=$(uname -s)
-[ "$OS" = "Darwin" ] && BASE_PATH="/opt/homebrew/opt/fzf/shell" || BASE_PATH="/usr/share/doc/fzf/examples"
+[ "$OS" = "Darwin" ] && BASE_PATH="/opt/homebrew/opt/fzf/shell" || BASE_PATH="/usr/share/fzf"
 [[ $- == *i* ]] && source "$BASE_PATH/completion.zsh" 2> /dev/null
 
 # Key bindings
